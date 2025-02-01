4fbe8d8aaf68aa2afd83b38833df2faab87fabdb
Mayank Dhiman <143904041+notimeforcaution@users.noreply.github.com>
2025-02-02 03:19:05 +0530

    Add TODOs in main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 def main():
-    # Initializers.
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     model = GPT2LMHeadModel.from_pretrained("gpt2")
 
-    # Token
     with open("medications.txt", "r") as f:
         text = f.read()
-    
-    inputs = tokenizer(text)
 
-    print(inputs)
-
-
-
+    # TODO(ntfc): Replicate the changes in notebook to over here, as much as possible.
 
 def __main__():
     main()
